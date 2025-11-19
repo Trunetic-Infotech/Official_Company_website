625c95dca2c0dd46c11e94eea9465a3b7e7513b5
anam-a15 <anam.a@truneticinfotech.com>
2025-11-19 16:22:57 +0530

    My local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import {BrowserRouter} from 'react-router-dom'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
     <BrowserRouter>
-    
-
+     
     </BrowserRouter>
-
- 
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
