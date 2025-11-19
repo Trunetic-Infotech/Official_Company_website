0b1781cdc3ae7c0f848d99d522fa0654b5a9ffa2
AmarKalel077 <AmaR.k@truneticinfotech.com>
2025-11-19 15:25:42 +0530

    Project Created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter} from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter>
+    
+
+    </BrowserRouter>
+
+ 
+    </>
+  )
+}
+
+export default App
